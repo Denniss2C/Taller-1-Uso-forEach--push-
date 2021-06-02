ae91ef5a5c50ea71aeac825762b7cceed28bd83c
Denniss2C <denniscaisa@gmail.com>
2021-06-02 11:31:23 -0500

    completar todavia

--- js/script-arrays.js
+++ js/script-arrays.js
@@ -1,53 +1,53 @@
-//ARREGLOS DE JAVASCRIPT
-//Arreglo: Un conjunto de elementos definidos de una estructura
-//DEFINICIONES
-//Arreglo de Strings
-var privincias = ['Cotopaxi',' Azuay',' Guayas',' EL Oro'];
-//Arreglo de varios tipos de datos
-var combinado = [20,' Dennis', true,' 2021-05-31'];
-//Definir un arreglo por la longitud de elementos
-var numeros_primos = new Array(11);
-//Definir un arreglo por el constructor con sus elementos
-var serie_fibonacci = new Array(1, 1, 2, 3, 5, 8, 13, 21);
-//Definir un areeglo vacio
-var figuras_geometricas = new Array();
+var personas = [];
 
-//Funcion que imprime los arreglos 
-function imprimirArreglos(){
-    document.getElementById('arrProvincias').innerText=privincias;
-    document.getElementById('arrCombinado').innerText=combinado;
-    document.getElementById('arrNumPrimos').innerText=numeros_primos;
-    document.getElementById('arrSerieFib').innerText=serie_fibonacci;
-    document.getElementById('arrFigGeo').innerText=figuras_geometricas;
+function imprimirPersonas(){
+    document.getElementById('arrPersonas').innerText = personas;
+    document.getElementById('arrPersonas').append(" El arreglo tiene "+ personas.length + " elementos");
 }
 
-function imprimirCadaProvincia(){
-    //con forEach se va a reconocer cada elemento del arreglo
-    privincias.forEach((x) => {
-        var li = document.createElement('li'); //Crea un nuevo elemento por su etiqueta HTML //<li></li>
-        li.innerText = x;
-        document.getElementById('ulProvincias').append(li);
-    }); //El metodo forEach es un callback, lo que quiere decir es que impoemnta una funcion
+function imprimirNuevoPersonas(NuevoPersonas){
+    document.getElementById('arrNuevoPersonas').innerText = nuevoPersonas;
+    document.getElementById('arrNuevoPersonas').append(" El arreglo tiene "+ personas.length + " elementos");
 }
 
-function imprimirFibonacci(){
-    serie_fibonacci.forEach((y) => {
-        var li = document.createElement('li'); 
-        li.innerText = y;
-        document.getElementById('ulSerieFib').append(li);
-    });
+function pushPersonas(){
+    var input = document.getElementById('txtNombre');
+    var nombre = input.value;
+    var numero_elementos = personas.push(nombre);
+    console.log("# de elementos en el arreglo = "+numero_elementos);
+    input.value = "";
+    imprimirPersonas();
 }
 
-function agregrarFigura(){
-    var nombre = document.getElementById('txtFigura').value;
-    figuras_geometricas.push(nombre);
-    document.getElementById('arrFigGeo').innerText=figuras_geometricas;
-    document.getElementById('txtFigura').value = '';
+function popPersonas(){
+    var eliminado = personas.pop();
+    console.log("Elemento eliminado = "+eliminado);
+    imprimirPersonas();
 }
 
-function agregrarProvincia(){
-    var agregar = document.getElementById('txtProvincias').value;
-    privincias.push(agregar);
-    document.getElementById('arrProvincias').innerText=privincias;
-    document.getElementById('txtProvincias').value = '';
+function ushiftPersonas(){
+    var input = document.getElementById('txtNombre');
+    var nombre = input.value;
+    var numero_elementos = personas.unshift(nombre);
+    console.log("# de elementos en el arreglo = "+numero_elementos);
+    input.value = "";
+    imprimirPersonas();
 }
+
+function shiftPersonas(){
+    var eliminado = personas.shift();
+    console.log("Elemento eliminado = "+eliminado);
+    imprimirPersonas();
+}
+
+function slicePersonas(){
+    var strInicio = document.getElementById('txtInicio').value;
+    var strFin = document.getElementById('txtFinal').value;
+    var intInicio = parseInt(strInicio);
+    var intFin = parseInt(strFin);
+    var nuevoArregloPersonas = personas.slice(intInicio,intFin);
+    imprimirNuevoPersonas(nuevoArregloPersonas);
+    imprimirPersonas();
+
+}
+
